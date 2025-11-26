850dbdb72f4416588d26717d9bd1bc8469ba70be
Damian Rojas <rojasdamianc@gmail.com>
2025-11-26 17:03:53 -0300

    Se agrega estilo a la pagina mi cuenta

--- tienda/wp-content/themes/storefront-child/woocommerce/myaccount/dashboard.php
+++ tienda/wp-content/themes/storefront-child/woocommerce/myaccount/dashboard.php
@@ -5,13 +5,16 @@ $current_user = wp_get_current_user();
 $nombre = $current_user->first_name ? $current_user->first_name : $current_user->display_name;
 ?>
 
-<div class="induvane-dashboard" style="padding: 10px 0;">
+<!-- <div class="induvane-dashboard" style="padding: 10px 0;"> -->
+
+<div class="induvane-myaccount-dashboard">
     
     <p style="font-size: 18px; font-weight: 600; margin-bottom: 10px;">
         ¡Hola <?php echo esc_html( $nombre ); ?>! Gracias por ser parte de <strong>InduVane</strong>.
     </p>
 
-    <p style="margin-bottom: 15px;">
+    <!-- <p style="margin-bottom: 15px;"> -->
+    <p class="induvane-myaccount-text">
         Desde tu cuenta tenés el control total de tus compras: revisá tus pedidos, actualizá tus direcciones
         y mantené tus datos siempre al día.
     </p>
@@ -22,3 +25,4 @@ $nombre = $current_user->first_name ? $current_user->first_name : $current_user-
     </p>
 
 </div>
+
